b092410f6994dc5726bf62d6cb1615a9085f06e5
n-dub <ragucoz@gmail.com>
2022-04-21 20:19:12 +0500

    Fixed compilation issue

--- UraniumVisualizer/SessionEvent.h
+++ UraniumVisualizer/SessionEvent.h
@@ -1,7 +1,6 @@
 #pragma once
-
 #include <vector>
-//#include <random>
+#include <cstdint>
 #include <EventType.h>
 
 namespace UN
--- UraniumVisualizer/main.cpp
+++ UraniumVisualizer/main.cpp
@@ -3,11 +3,11 @@
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     UN::Print(UN::ProfilingSession::ToString(UN::ProfilingSession::GetFakeProfilingSession()));
-    return a.exec();
+    return QApplication::exec();
 }
